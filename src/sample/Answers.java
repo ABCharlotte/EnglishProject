19de497811564beeadaa18a3da27fcae94a363a7
ABCharlotte <charlotte.boulle@telecomnancy.net>
2018-02-26 16:27:39 +0100

    question_answer

---
+++ src/sample/Answers.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Answers {
+    private int num_a;
+    private boolean visibility;
+    private String[] accepted= new String[10];
+    private String[] hint = new String[2];
+}
---
+++ src/sample/Question.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Question {
+    private int num_q;
+    private String theme;
+    private String nature; //{"image", "text"}
+    private Answers[] answers;
+}
