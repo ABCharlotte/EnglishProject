f14989023666faaf28bc8edc305e9d871bc86258
ABCharlotte <charlotte.boulle@telecomnancy.net>
2018-02-26 20:34:07 +0100

    class question and answers

--- src/sample/Answers.java
+++ src/sample/Answers.java
@@ -3,6 +3,38 @@ package sample;
 public class Answers {
     private int num_a;
     private boolean visibility;
-    private String[] accepted= new String[10];
-    private String[] hint = new String[2];
+    private String[] accepted;
+    private String[] hint;
+
+    public void Answers(){
+        accepted= new String[10];
+        hint = new String[2];
+    }
+
+    public void Answers(int newNum_a, boolean newVisibility, String[] newAccepted, String[] newHint){
+        num_a =newNum_a;
+        visibility=newVisibility;
+        accepted=newAccepted;
+        hint=newHint;
+    }
+
+    public int getNum_a() {
+        return num_a;
+    }
+
+    public boolean isVisibility() {
+        return visibility;
+    }
+
+    public String[] getAccepted() {
+        return accepted;
+    }
+
+    public String[] getHint() {
+        return hint;
+    }
+
+    public void setVisibility(boolean visibility) {
+        this.visibility = visibility;
+    }
 }
--- src/sample/Question.java
+++ src/sample/Question.java
@@ -3,6 +3,39 @@ package sample;
 public class Question {
     private int num_q;
     private String theme;
-    private String nature; //{"image", "text"}
+    private String nature; //{"text" or "image"}
+    private String question; //{text or path of the image}
     private Answers[] answers;
+
+    public Question(int num_q, String theme) {
+        this.num_q = num_q;
+        this.theme = theme;
+    }
+    public Question(int num_q, String theme, String nature, String question, Answers[] answers) {
+        this.num_q = num_q;
+        this.theme = theme;
+        this.nature = nature;
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public int getNum_q() {
+        return num_q;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public String getNature() {
+        return nature;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public Answers[] getAnswers() {
+        return answers;
+    }
 }
