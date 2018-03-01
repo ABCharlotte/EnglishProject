7036644bbaf8b27c0d8864ddbb88e230bb88a370
ABCharlotte <charlotte.boulle@telecomnancy.net>
2018-03-01 19:35:08 +0100

    fix pb load

--- src/sample/LoadQandA.java
+++ src/sample/LoadQandA.java
@@ -11,20 +11,24 @@ import java.io.IOException;
 
 public class LoadQandA {
     private static final String filePath = "src/data/questions.json";
-    private static int nbOfQuest=2;
+    //private static int nbOfQuest=3;
 
     public  static Question[] loadQuestions(){
-        Question[] questions = new Question[nbOfQuest];
+        Question[] questions = new Question[0];
         try {
             FileReader reader = new FileReader(filePath);
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonQuest = (JSONObject) jsonParser.parse(reader);
-            for (int i = 1; i <= nbOfQuest ; i++) {
+            int nbOfQuest=jsonQuest.size();
+            questions = new Question[nbOfQuest];
+            for (int i = 0; i < nbOfQuest ; i++) {
                 JSONObject quest= (JSONObject) jsonQuest.get(""+i+"");
+                //System.out.println(i);
                 String theme = (String) quest.get("theme");
-                boolean val = quest.get("validated").equals("True");
+                //System.out.println("theme question "+i+":"+quest.get("theme"));
+                boolean val = quest.get("validated").equals(true);
                 Question question = new Question(i,theme, val);
-                questions[i-1]=question;
+                questions[i]=question;
             }
 
         } catch (ParseException e) {
@@ -43,7 +47,7 @@ public class LoadQandA {
             JSONObject jsonQuest = (JSONObject) jsonParser.parse(reader);
             JSONObject quest = (JSONObject) jsonQuest.get(""+num_q+"");
             String theme = (String) quest.get("theme");
-            boolean val = quest.get("validated").equals("True");
+            boolean val = quest.get("validated").equals(true);
             String nature = (String) quest.get("nature");
             String text = (String) quest.get("question");
             JSONArray ansArray = (JSONArray) quest.get("answers");
@@ -51,7 +55,7 @@ public class LoadQandA {
             for (int i = 0; i < ansArray.size() ; i++) {
                 JSONObject ans= (JSONObject) ansArray.get(i);
                 int num_a = (int) ((long)ans.get("number"));
-                boolean vis = ans.get("visibility").equals("True");
+                boolean vis = ans.get("visibility").equals(true);
                 JSONArray ansArr = (JSONArray) ans.get("answer");
                                 String[] accepted = new String[ansArr.size()];
                 for (int j = 0; j < ansArr.size(); j++) {
@@ -79,8 +83,8 @@ public class LoadQandA {
         LoadQandA L = new LoadQandA();
         Question[] q= L.loadQuestions();
         System.out.println(q[0].getTheme());
-        Question qAndA = L.loadAnswers(2);
-        System.out.println(qAndA.getQuestion());
+        Question qAndA = L.loadAnswers(0);
+        System.out.println(qAndA.getAnswers()[1].getAccepted()[0]);
         /*
 
         //Gson g = new Gson();
