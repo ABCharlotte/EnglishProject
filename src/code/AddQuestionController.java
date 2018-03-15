4cbe2c3da14d2352c6bf6f12b026160ae6441725
charl04 <charles.valensole@gmail.com>
2018-03-15 18:59:02 +0100

    Added load and questionForm UI

--- src/code/AddQuestionController.java
+++ src/code/AddQuestionController.java
@@ -1,4 +1,37 @@
 package code;
 
-public class AddQuestionController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class AddQuestionController extends MenuController {
+    public BorderPane borderPane;
+    public VBox leftVbox;
+    public VBox rightVbox;
+    public TextField questionTextField;
+    public Button cancelButton;
+    public Button okButton;
+    private int questionNumber=1;
+    //public Button plusButton= new Button("+").setOnAction(event -> addQuestion());
+
+    @Override
+    void initialize() {
+        //this.leftVbox.getChildren().add(0,this.plusButton);
+    }
+
+    @FXML
+    private void handleOK(){
+
+    }
+    @FXML
+    private void handleCancel(){
+
+    }
+    private void addQuestion(){
+        this.leftVbox.getChildren().remove(0);
+        this.leftVbox.getChildren().addAll(new Label("Question "+this.questionNumber),new TextField());
+    }
 }
